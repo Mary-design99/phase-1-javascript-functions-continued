8729643f313092a209c4272575ade889a9b77ab3
Mary-design99 <mary.nyarangi@student.moringaschool.com>
2025-06-08 21:48:37 +0300

    continue

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+function mondayWork(activity="go to the office")
+{
+    return`This Monday, I will ${activity}.`;
+
+
+
+
+}
+function wrapAdjective(flair=""){
+    return function(adjective="special"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+
+}
+const encouragingPromptFunction=wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer!"));
+console.log(saturdayFun("roller-skate"));
+console.log(mondayWork("go to the office."))
